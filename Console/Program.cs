d78b332e67fa26d1e870acbe4751c4433b87783c
Berfin Gultekin <gberfin783@gmail.com>
2023-10-23 18:56:53 +0300

    c#
    Kodlar yüklendi.

---
+++ Console/Program.cs
@@ -0,0 +1,65 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using System;
+
+namespace Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //IColorService colorService = new ColorManager(new EfColordal());
+            //ICarService carService = new CarManager(new EfCarDal());
+            IBrandService brandService = new BrandManager(new EfBrandDal());
+
+            //Add(carService);
+            //AddColor(colorService);
+            Add(brandService);
+
+            //carService.GetAll();
+            //colorService.GetAll();
+            //brandService.GetAll();
+
+            //static void Add(ICarService carService)
+            //{
+            //    carService.Add(new Car
+            //    {
+            //        DailyPrice = 2000,
+            //        Name = "RangeRover",
+            //        BrandId = 1,
+            //        ColorId = 1,
+            //        Description = "cool car",
+            //        ID = 1,
+            //        ModelYear = 2023,
+
+            //    });
+
+
+
+            //}
+
+          
+           // static void AddColor(IColorService colorService)
+           //{
+           //     colorService.Add(new Color
+           //     {
+           //       ID = 2,
+           //       Name = "Purple"
+
+           //     });
+                
+           // }
+            static void Add(IBrandService brandService)
+            {
+                brandService.Add(new Brand
+               {
+                    ID=1,Name="Land Rove"
+
+              });
+
+            }
+        }
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -0,0 +1,40 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfCarDal : EfEntityRepositoryBase<Car, RentCarDBContext>, ICarDal
+    {
+        
+        public List<CarsDetailDTO> GetCarDetails()
+        {
+            using(RentCarDBContext context= new RentCarDBContext())
+            {
+                var result = from c in context.Cars
+                             join co in context.Colors
+                             on c.ColorId equals co.ID
+                             join b in context.Brands
+                             on c.ID equals b.ID
+                             select new CarsDetailDTO
+                             {
+                                BrandName=b.Name,
+                                CarName=c.Name,
+                                ColorName=co.Name,
+                                DailyPrice=c.DailyPrice
+
+                             };
+                return result.ToList();
+
+            }
+        }
+    }
+}
